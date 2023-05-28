bc30c74fd9a33e9f0cd33718aab06a6045c6245f
Alexandre Stumpf <als.stumpf@gmail.com>
2023-05-28 03:06:46 -0300

    operadores aritmeticos

---
+++ cursoAdaJs/operadores-aritmeticos.js
@@ -0,0 +1,20 @@
+let num1 = 20;
+let num2 = 2;
+
+//Adição +
+//Subtração -
+//Multiplicação *
+//Divisão /
+//Exponencial **
+//Resto da divisão %
+
+console.log("Soma = ", num1 + num2);
+console.log("Subtração = ", num1 - num2);
+console.log("Multiplicação = ", num1 * num2);
+console.log("Divisão = ", num1 / num2);
+console.log("Exponenciação = ", num1 ** num2);
+console.log("Resto da divisão = ", num1 % num2);
+
+//biblioteca Math
+console.log(Math.PI);//numero do pi
+console.log(Math.sqrt(16))//raiz quadrada
