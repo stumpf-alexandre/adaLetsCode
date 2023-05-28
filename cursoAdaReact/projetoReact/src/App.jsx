acfc055a507402086a576c0a107888f4e419f6c3
Alexandre Stumpf <als.stumpf@gmail.com>
2023-05-28 20:52:02 -0300

    components e props no react

--- cursoAdaReact/projetoReact/src/App.jsx
+++ cursoAdaReact/projetoReact/src/App.jsx
@@ -1,8 +1,25 @@
+import MeuComponente from "./components/MeuComponents"
 
 function App() {
 
   return (
-    <h1>Olá Mundo!!! No React</h1>
+    <div>
+      <h1>Olá Mundo!!! No React</h1>
+
+      <MeuComponente/>
+      <MeuComponente/>
+
+      <MeuBotao conteudo='Me click'/>
+      <MeuBotao conteudo='Depois aqui'/>
+      <MeuBotao conteudo='E depois aqui'/>
+    </div>
+  )
+}
+function MeuBotao(props) {
+  return (
+
+    // eslint-disable-next-line react/prop-types
+    <button>{props.conteudo}</button>
   )
 }
 
