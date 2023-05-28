df2f5b93238c7c3f3852276bc7be786dfb8f7220
Alexandre Stumpf <als.stumpf@gmail.com>
2023-05-28 02:53:12 -0300

    variaveis com javascript

---
+++ cursoAdaJs/variaveis.js
@@ -0,0 +1,26 @@
+//Variáveis
+
+let idade = 43;
+
+console.log("Sua idade é: " + idade);
+
+//Tipos de variaveis
+let numero = 30;//variavel do tipo number
+let altura = 1.85;//variavel do tipo number
+let nome = 'Alexandre';//variavel do tipo String (texto)
+let estudando = true;//variavel do tipo Booleano (true ou false)
+
+console.log(numero, typeof numero);
+console.log(altura, typeof altura);
+console.log(nome, typeof nome);
+console.log(estudando, typeof estudando);
+
+let outra;//variavel indefinida
+let nula = null;//variavel nula
+const constante = 100;//não pode ser atribuido outro valor, além do que voce atribuiu de inicio
+const fixo = "eu fiz o almoço";
+
+console.log(outra, typeof outra);
+console.log(nula, typeof nula);
+console.log(constante, typeof constante);
+console.log(fixo, typeof fixo);
