7b21ceb29b35927bb005823872b79e4ac0d883ec
Alexandre Stumpf <als.stumpf@gmail.com>
2023-05-28 04:17:16 -0300

    estruturas condicionais

---
+++ cursoAdaJs/estrutura-condicional.js
@@ -0,0 +1,16 @@
+const idade = 20;
+
+if (idade >= 18) {
+    console.log("Você é maior de idade");
+} else {
+    console.log("Você é menor de idade");
+}
+
+let media = 5;
+if (media >= 7) {
+    console.log("Você esta aprovado");
+} else if (media >= 5) {
+    console.log("Você esta em recuperação");
+} else {
+    console.log("Você esta reprovado");
+}
